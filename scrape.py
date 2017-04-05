008d3c3e4a103a9ef7f6df60402d7ed7b3747433
craftsmon <craftsmon@outlook.com>
2017-04-05 15:08:09 +0300

    Implement link scrapper

---
+++ scrape.py
@@ -0,0 +1,22 @@
+import requests
+from bs4 import BeautifulSoup
+
+url = "http://headacheaustralia.org.au/types-of-headaches/"
+dom_element = { "tag": "div", "class": "disorder-links", "id": ""}
+
+# parse url
+# arguments are @url
+# @dictionary containing attribute to dom element with the links
+# return href links
+def get_links(url, dom_element):
+    # get page and get and object
+    # then parse
+    page = requests.get(url)
+    soup = BeautifulSoup(page.content, 'html.parser')
+
+    div = soup.find(dom_element["tag"], attrs={"class":dom_element["class"]})
+    links = [div.find('a')['href'] for a in div]
+
+    return links
+
+print(get_links(url,dom_element))
